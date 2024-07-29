d12adef6107c85cbb01a3cc38e0cfeab796bcd49
Nehal <m.nehalq125@gmail.com>
2024-07-28 21:04:35 -0700

    add styling

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,25 @@
 import { auth, signInWithEmailAndPassword } from "./firebase.js"
 
 const form = document.querySelector("#loginForm")
+const errorMsg = document.querySelector("#error")
+const showPassword = document.querySelector("#showPassword") 
+const password = document.querySelector("#password")
+
+showPassword.addEventListener("click" , (e) => {
+    console.log(event.target.checked);
+    if (event.target.checked) {
+        password.type = "text"
+        console.log("🚀 ~ showPassword.addEventListener ~ password:", password)
+    } else {
+        password.type = "password"
+    }
+} )
 
 form.addEventListener('submit', async (event) => {
     try {
         event.preventDefault();
-        const email = event.target.children[0].value
-        const password = event.target.children[2].value
+        const email = document.querySelector("#email").value
+        const password = document.querySelector("#password").value
 
         const result = await signInWithEmailAndPassword(auth , email, password)
         console.log("🚀 ~ form.addEventListener ~ result:", result)
@@ -14,5 +27,6 @@ form.addEventListener('submit', async (event) => {
         window.location = "./dashboard.html"
     } catch (error) {
     console.log("🚀 ~ form.addEventListener ~ error:", error.message)
+    errorMsg.innerHTML = "Invalid User Email or Password"
     }
 })
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,11 +1,24 @@
 import { auth, createUserWithEmailAndPassword } from './firebase.js'
 let signupForm = document.querySelector('.signupForm')
+const showPassword = document.querySelector("#showPassword") 
+const password = document.querySelector("#password")
+const errorMsg = document.querySelector("#error")
+
+showPassword.addEventListener("click" , (e) => {
+    console.log(event.target.checked);
+    if (event.target.checked) {
+        password.type = "text"
+        console.log("🚀 ~ showPassword.addEventListener ~ password:", password)
+    } else {
+        password.type = "password"
+    }
+} )
 
 signupForm.addEventListener('submit', async (event) => {
     try {
         event.preventDefault()
-        const email = event.target.children[0].value
-        const password = event.target.children[2].value
+        const email = document.querySelector("#email").value
+        const password = document.querySelector("#password").value
         // console.log("🚀 ~ signupForm.addEventListener ~ email:", email.value)
         // console.log("🚀 ~ signupForm.addEventListener ~ password:", password.value)
 
@@ -13,7 +26,8 @@ signupForm.addEventListener('submit', async (event) => {
         console.log("🚀 ~ signupForm.addEventListener ~ result:", result)
         alert('Congratulation! You are register')
         window.location = "./login.html"
-    } catch (error) {
+     } catch (error) {
         alert('error', error.message);
+        errorMsg.innerHTML = "Invalid User Email or Password"
     }
 })
